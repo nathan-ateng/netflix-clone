06a3e75d4858d829b76b1e5627255f14120cddd7
Nathan Ateng <nathanateng@gmail.com>
2023-09-25 18:31:29 +0300

    add show netflix originals to UI

--- script.js
+++ script.js
@@ -3,14 +3,49 @@
 // APP: Netflix Clone
 // SOLVE: Using an API pull all movies and update the application UI using response data
 
+// Call main functions when the page is loaded
+window.onload = () => {
+  getOriginals();
+  getTrendingNow();
+  getTopRated();
+};
 // Create fetchMovies function that makes a dynamic API call to what is needed
 
-function fetchMovies(url, dom_ele, path_type) {
+function fetchMovies(url, dom_element, path_type) {
   // Use fetch with url
-  const fetchMovies = fetch(url)
-    .then((response) => response.json())
-    .then((data) => {});
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((data) => {
+      showMovies(data, dom_element, path_type);
+    })
+    .catch((error) => console.log(error));
 }
+
+// Functon that displays the movies to the DOM
+const showMovies = (movies, dom_element, path_type) => {
+  // Create a variable that grabs id or class
+  let moviesEl = document.querySelector(dom_element);
+
+  // Loop through movies object
+  for (let movie of movies.results) {
+    let imageElement = document.createElement("img");
+
+    // Set attributes
+    imageElement.setAttribute("data-id", movie.id);
+
+    // Set source
+    imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`;
+
+    // Append the imageElement to the DOM element
+    moviesEl.appendChild(imageElement);
+  }
+};
 
 // Function that fetches Netflix originals
 function getOriginals() {
